606d18c20a1ee483035562403ed167c2461da292
matan515 <diz.matan@gmail.com>
2025-11-03 10:25:28 +0200

    i start the code for the motor of the subsystam

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,12 @@
 
 package frc.robot;
 
+import frc.demacia.utils.Motors.BaseMotorConfig.Canbus;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * globally (i.e. public static). Do not putcu anything functional in this class.
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
@@ -15,5 +17,8 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final String roborioId = "rio";
   }
+
+  public Canbus roborioId;
 }
