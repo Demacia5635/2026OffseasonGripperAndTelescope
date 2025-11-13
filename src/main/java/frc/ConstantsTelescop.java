5a5c4ec59ec6b0ab2595b19c190cedd8b0d833af
matan515 <diz.matan@gmail.com>
2025-11-13 23:10:24 +0200

    i cange ConstanstsTelescope

---
+++ src/main/java/frc/ConstantsTelescop.java
@@ -0,0 +1,47 @@
+package frc;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.telescop;
+
+public class ConstantsTelescop {
+
+    private telescop telescop = new telescop();
+
+    public static final double RPM = 6000;
+    public static final double maxVelocity = (Math.PI * 2) * RPM;
+
+    private double lastVelocity = 0;
+    private double lastTime = Timer.getFPGATimestamp();
+    private double lastAcceleration = 0;
+
+    private double maxAcceleration = 0;
+    private double jerk = 0;
+
+    public void update() {
+        double currentTime = Timer.getFPGATimestamp();
+        double currentVelocity = telescop.currentVelocity();
+
+        double deltaV = currentVelocity - lastVelocity;
+        double deltaT = currentTime - lastTime;
+
+        double acceleration = deltaV / deltaT;
+        jerk = (acceleration - lastAcceleration) / deltaT;
+
+        if(acceleration > maxAcceleration){
+            maxAcceleration = acceleration;
+        }
+
+ 
+        lastVelocity = currentVelocity;
+        lastTime = currentTime;
+        lastAcceleration = acceleration;
+    }
+
+    public double getMaxAcceleration() {
+        return maxAcceleration;
+    }
+
+    public double getJerk() {
+        return jerk;
+    }
+}
--- src/main/java/frc/robot/subsystems/telescop.java
+++ src/main/java/frc/robot/subsystems/telescop.java
@@ -4,24 +4,53 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.demacia.utils.Motors.TalonConfig;
 import frc.demacia.utils.Motors.TalonMotor;
+import frc.ConstantsTelescop;
 import frc.demacia.utils.Motors.BaseMotorConfig.Canbus;
+import frc.robot.Constants;
 
 public class telescop extends SubsystemBase {
   /** Creates a new telescop. */
-  public telescop() {}
+  public telescop() {
+
+  }
 
   private TalonMotor telescopMotor;
+  final double maxJerk;  
 
   private void subsystemsTelescope(){
-    TalonConfig config = new TalonConfig(0, Canbus.Rio, "telescopMotor");
+    TalonConfig config = new TalonConfig(0, Canbus.Rio, "telescopMotor")
+    .withBrake(false)
+    .withVolts(12)
+    .withDegreesMotor(14)
+    .withPID(0, 0, 0, 0, 0, 0, 0)
+    .withMotionParam(ConstantsTelescop.maxVelocity, ConstantsTelescop.maxAcceleration);
     
+
     telescopMotor = new TalonMotor(config);
   }
 
-    
+
+  public void close(){
+    telescopMotor.setMotion(0);
+  }
+
+  public void open(){
+    telescopMotor.setMotion(1.35);
+  }
+
+
+  public double currentVelocity(){
+    double currentVelocity = telescopMotor.getCurrentVelocity();
+    return currentVelocity;
+}
+
+
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
