1cd48790d3a482c65ade48d3be528afe158934f5
matan515 <diz.matan@gmail.com>
2025-11-14 13:14:17 +0200

    i crate command for the telescop

--- src/main/java/frc/Telescop/subsystems/telescop.java
+++ src/main/java/frc/Telescop/subsystems/telescop.java
@@ -1,9 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.Telescop.subsystems;
-
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.Telescop.ConstantsTelescop;
@@ -14,50 +9,51 @@ import frc.Telescop.Constants;
 
 public class telescop extends SubsystemBase {
 
-  ConstantsTelescop constants = new ConstantsTelescop();
-  Constants mainConstants = new Constants();
-  /** Creates a new telescop. */
-  public telescop() {}
+    ConstantsTelescop constants = new ConstantsTelescop();
+    Constants mainConstants = new Constants();
 
-  private TalonMotor telescopMotor;
+    private TalonMotor telescopMotor;
 
-  private void subsystemsTelescope(){
-    TalonConfig config = new TalonConfig(0, Canbus.Rio, "telescopMotor")
-    .withBrake(false)
-    .withVolts(12)
-    .withDegreesMotor(14)
-    .withPID(0, 0, 0, 0, 0, 0, 0)
-    .withMotionParam(constants.getMaxVelocity(), constants.getMaxAcceleration(), constants.getMaxJerk());
-    
+    /** Creates a new telescop. */
+    public telescop() {
+        subsystemsTelescope();
+    }
 
-    telescopMotor = new TalonMotor(config);
-  }
+    private void subsystemsTelescope() {
+        TalonConfig config = new TalonConfig(0, Canbus.Rio, "telescopMotor")
+            .withBrake(false)
+            .withVolts(12)
+            .withDegreesMotor(14)
+            .withPID(0, 0, 0, 0, 0, 0, 0)
+            .withMotionParam(constants.getMaxVelocity(), constants.getMaxAcceleration(), constants.getMaxJerk());
 
-
-  if (mainConstants.getState() == "open"){
-    telescopMotor.setMotion(constants.open);
-  }else if(mainConstants.getState() == "close"){
-    telescopMotor.setMotion(constants.close);
-  }else if(mainConstants.getState() == "L1"){
-    telescopMotor.setMotion(constants.getL1());
-  }else if(mainConstants.getState() == "L2"){
-    telescopMotor.setMotion(constants.getL2());
-  }else if(mainConstants.getState() == "L3"){
-    telescopMotor.setMotion(constants.getL3());
-  }else if(mainConstants.getState() == "L4"){
-    telescopMotor.setMotion(constants.getL4());
-  }
-
-  
-  public double currentVelocity(){
-    double currentVelocity = telescopMotor.getCurrentVelocity();
-    return currentVelocity;
-}
+        telescopMotor = new TalonMotor(config);
+    }
 
 
+    public void updateState() {
+        switch (mainConstants.getState()) {
+            case "open":
+                telescopMotor.setMotion(constants.open);
+            case "close":
+                telescopMotor.setMotion(constants.close);
+            case "L1":
+                telescopMotor.setMotion(constants.getL1());
+            case "L2":
+                telescopMotor.setMotion(constants.getL2());
+            case "L3":
+                telescopMotor.setMotion(constants.getL3());
+            case "L4":
+                telescopMotor.setMotion(constants.getL4());
+        }
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public double currentVelocity() {
+        return telescopMotor.getCurrentVelocity();
+    }
+
+    @Override
+    public void periodic() {
+        updateState();
+    }
 }
