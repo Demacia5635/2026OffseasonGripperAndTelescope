49cf2c7622e71dbdc757a17c164eead9eadb0be3
UriMeyTal <urimeytal100@gmail.com>
2025-12-15 15:53:35 +0200

    add average function

--- src/main/java/frc/demacia/utils/Sensors/UltraSonicSensor.java
+++ src/main/java/frc/demacia/utils/Sensors/UltraSonicSensor.java
@@ -1,9 +1,11 @@
 package frc.demacia.utils.Sensors;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import frc.demacia.utils.Log.LogManager;
+import java.util.ArrayDeque;
+import java.util.Queue;
 public class UltraSonicSensor extends Ultrasonic implements AnalogSensorInterface{
     public UltraSonicSensor( UltraSonicSensorConfig config) {
-        super(config.pingChannel, config.channel);
+        super(config.pingChannel, config.echoChannel);
         this.config = config;
         name = config.name;
         addLog();
@@ -26,10 +28,29 @@ public class UltraSonicSensor extends Ultrasonic implements AnalogSensorInterfac
     }
 
     public double getRangeMeters() {
-        return getRangeMM() / 1000 ;
+        return getRangeMM() / 1000.0 ;
+    }
+
+    
+    private static final int average_Window = 5;
+    private final Queue<Double> samples = new ArrayDeque<>();
+    private double sum = 0;
+
+    public double getAverage() {
+        double current = getRangeMeters();
+        samples.add(current);
+        sum += current;
+
+        if (samples.size() > average_Window) {
+            sum -= samples.remove();
+        }
+
+        return sum / samples.size();
     }
 
     private void addLog() {
         LogManager.addEntry(name + "range", () -> getRangeMeters(), 3);
+        LogManager.addEntry(name + "avg range", () -> getAverage(), 3);
+
     }
 }
